92937f99cd5dad9c7d273966dbc895fee0057f14
Sheeri-Fatima <shizasiddique918@gmail.com>
2025-08-22 18:33:39 +0530

    banner update

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,18 +5,18 @@ import "swiper/css/effect-fade";
 import "swiper/css/pagination";
 import Img1 from "/images/banner1.jpg";
 import Img2 from "/images/banner2.jpg";
-import Img3 from "/images/banner3.jpg";
+import Img3 from "/images/banner4.jpg";
 
 
 const slides = [
   {
-    image: Img1,
+    image: Img3,
   },
   {
     image: Img2,
   },
   {
-    image: Img3,
+    image: Img1,
   },
 ];
 
@@ -24,28 +24,49 @@ const Banner = () => {
 
 
   return (
-    <section className="relative w-full h-[90vh] overflow-hidden">
-      <Swiper
-        modules={[Autoplay, Pagination, EffectFade]}
-        autoplay={{ delay: 4000, disableOnInteraction: false }}
-        pagination={{ clickable: true }}
-        effect="fade"
-        loop
-        className="w-full h-full"
-      >
-        {slides.map((slide, index) => (
-          <SwiperSlide key={index}>
-            <img
-              src={slide.image}
-              loading="lazy"
-              className="w-full h-full object-cover"
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    // <section className="relative w-full h-[90vh] mt-20 overflow-hidden">
+    //   <Swiper
+    //     modules={[Autoplay, Pagination, EffectFade]}
+    //     autoplay={{ delay: 4000, disableOnInteraction: false }}
+    //     pagination={{ clickable: true }}
+    //     effect="fade"
+    //     loop
+    //     className="w-full h-full"
+    //   >
+    //     {slides.map((slide, index) => (
+    //       <SwiperSlide key={index}>
+    //         <img
+    //           src={slide.image}
+    //           loading="lazy"
+    //           className="w-full h-full object-fill"
+    //         />
+    //       </SwiperSlide>
+    //     ))}
+    //   </Swiper>
 
-      {/* Floating Animation Elements */}
-    </section>
+    //   {/* Floating Animation Elements */}
+    // </section>
+    <section className="relative w-full h-[80vh] mt-20 overflow-hidden">
+  <Swiper
+    modules={[Autoplay, Pagination, EffectFade]}
+    autoplay={{ delay: 4000, disableOnInteraction: false }}
+    pagination={{ clickable: true }}
+    effect="fade"
+    loop
+    className="w-full h-full"
+  >
+    {slides.map((slide, index) => (
+      <SwiperSlide key={index}>
+        <img
+          src={slide.image}
+          loading="lazy"
+          className="w-full h-full object-cover"
+        />
+      </SwiperSlide>
+    ))}
+  </Swiper>
+</section>
+
   );
 };
 
